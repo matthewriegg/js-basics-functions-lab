4fcc800122c2bb3a37f1b8c09543469cebaa5236
Matthew Riegg <matthew.d.riegg@gmail.com>
2021-05-17 18:13:43 -0700

    commit tree 97c06b03600cfacbcc714f28283bbb1a5bf4acad

--- index.js
+++ index.js
@@ -56,10 +56,10 @@ function calculatesFarePrice(start, destination) {
         return 0;
         }
 
-    else if ((distance) >= 400 && distance < 2000){
+    else if (distance >= 400 && distance < 2000){
         return 0.02*(distance - 400);
         }
-    else if ((distance) > 2000 && distance < 2500){
+    else if (distance > 2000 && distance < 2500){
         return 25;
         } 
 
